97caecb791abeae3b52e4e4ba1ed617251747913
Mahtab Rezaei <s3594202@student.rmit.edu.au>
2019-09-24 23:12:46 +1000

    put the form thing back, console log on submit

--- src/modules/company/containers/CreateNewJob.tsx
+++ src/modules/company/containers/CreateNewJob.tsx
@@ -5,25 +5,32 @@ import { EUserType } from '../../../models/UserType'
 const CreateNewJob = () => {
     const [ newJob, setNewJob ] = useState({});
 
-    const handleChange = (event: any) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setNewJob({...newJob, [event.target.name]: event.target.value });
     }
 
-    return (<div>
-        <div>
-            <label>Job title</label>
-            <input type="text" name="job-title" onChange={handleChange}/>
-        </div>
-        <div>
-            <label>Company name</label>
-            <input type="text" name="company-name" onChange={handleChange}/>
-        </div>
-        <div>
-            <label>Description</label>
-            <input type="textarea" name="description" onChange={handleChange}/>
-        </div>
-        <button onClick={() => console.log(newJob)}>Create</button>
-    </div>);
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        console.log("creating new job with...", newJob);
+        event.preventDefault();
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label>Job title</label>
+                <input type="text" name="job-title" onChange={handleChange}/>
+            </div>
+            <div>
+                <label>Company name</label>
+                <input type="text" name="company-name" onChange={handleChange}/>
+            </div>
+            <div>
+                <label>Description</label>
+                <input type="textarea" name="description" onChange={handleChange}/>
+            </div>
+            <input type="submit" value="Create"/>
+        </form>
+    );
 };
 
 export default pageWrapper(CreateNewJob, { authorisedUserTypes: [EUserType.Company] })
