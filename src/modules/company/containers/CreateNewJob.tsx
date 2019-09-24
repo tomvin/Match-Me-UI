302a8539e05902f4e9da816c6dff27b4db88b7df
Mahtab Rezaei <s3594202@student.rmit.edu.au>
2019-09-24 23:06:33 +1000

    basic form to create new job

--- src/modules/company/containers/CreateNewJob.tsx
+++ src/modules/company/containers/CreateNewJob.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import pageWrapper from '../../shared/components/PageWrapper/PageWrapper'
 import { EUserType } from '../../../models/UserType'
 
 const CreateNewJob = () => {
-    return <div>
-        <p>Create new job page</p>
-    </div>;
+    const [ newJob, setNewJob ] = useState({});
+
+    const handleChange = (event: any) => {
+        setNewJob({...newJob, [event.target.name]: event.target.value });
+    }
+
+    return (<div>
+        <div>
+            <label>Job title</label>
+            <input type="text" name="job-title" onChange={handleChange}/>
+        </div>
+        <div>
+            <label>Company name</label>
+            <input type="text" name="company-name" onChange={handleChange}/>
+        </div>
+        <div>
+            <label>Description</label>
+            <input type="textarea" name="description" onChange={handleChange}/>
+        </div>
+        <button onClick={() => console.log(newJob)}>Create</button>
+    </div>);
 };
 
 export default pageWrapper(CreateNewJob, { authorisedUserTypes: [EUserType.Company] })
