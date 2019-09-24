7a7aed9755f5691d314024d8cd09101edb8ae8f5
Mahtab Rezaei <s3594202@student.rmit.edu.au>
2019-09-24 23:27:27 +1000

    next steps of comments

--- src/modules/company/containers/CreateNewJob.tsx
+++ src/modules/company/containers/CreateNewJob.tsx
@@ -3,30 +3,43 @@ import pageWrapper from '../../shared/components/PageWrapper/PageWrapper'
 import { EUserType } from '../../../models/UserType'
 
 const CreateNewJob = () => {
-    const [ newJob, setNewJob ] = useState({});
+    const [ newJob, setNewJob ] = useState({}); // newJob = { description: "abc" }
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setNewJob({...newJob, [event.target.name]: event.target.value });
+        // {
+        //     "jobTitle": "abc",
+        //     "description": "awesome job",
+        //     "companyName": "company"
+        // }
     }
 
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         console.log("creating new job with...", newJob);
+        // transform data to look like what the DB needs
+        // {
+        //     job: {
+        //         title: newJob.jobTitle
+        //     }
+        // }
+        // send data to db
         event.preventDefault();
+        // redirect to job postings page
     }
 
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>Job title</label>
-                <input type="text" name="job-title" onChange={handleChange}/>
+                <input type="text" name="jobTitle" onChange={handleChange}/>
             </div>
             <div>
                 <label>Company name</label>
-                <input type="text" name="company-name" onChange={handleChange}/>
+                <input type="text" name="companyName" onChange={handleChange}/>
             </div>
             <div>
                 <label>Description</label>
-                <input type="textarea" name="description" onChange={handleChange}/>
+                <input type="text" name="description" onChange={handleChange}/>
             </div>
             <input type="submit" value="Create"/>
         </form>
